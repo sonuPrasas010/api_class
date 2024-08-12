2df5af725f79446ffeb51f279ba5df0d85f3bf6a
sonu dhakal <sonudhakal010@gmail.com>
2024-08-12 22:39:38 +0545

    changes

---
+++ lib/shared/consts/consts.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Colors.green;
+final secondaryColor = Colors.red[400];
+
+const baseUrl = "https://api.thecatapi.com/v1";
+const assetsUrl = "https://thecatapi.com/v1/";
+
+const apiToken = "live_yGcx2OR1wG5V45S98jTHoeJ36W0NkexdjX08VZ5ubJiRJ50PzF3HJvd90IESLN6c";
+
+const borderRoundness = 0;
+const padding = EdgeInsets.symmetric(horizontal: 20, vertical: 5);
+
+
